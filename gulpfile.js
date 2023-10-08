1d8df6f7b3a43604bd15ab506738fd5f7ccada0b
Vasya Ryzhkoff <ryzh.v.n@mail.ru>
2023-10-08 23:18:34 +0700

    preliminary

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const { src, dest, series, watch, parallel } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const clean = require('gulp-clean');
+const browserSync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+
+function buildSass() {
+    return src('src/styles/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ includePaths: ['./node_modules'] }).on('error', sass.logError))
+        .pipe(
+            postcss([
+                autoprefixer({
+                    grid: true,
+                    overrideBrowserslist: ['last 2 versions']
+                }),
+                cssnano()
+            ])
+        )
+        .pipe(sourcemaps.write())
+        .pipe(dest('src/compiled-styles'))
+        .pipe(dest('dist/compiled-styles'))
+        .pipe(browserSync.stream());
+}
+
+function buildHtml() {
+    return src('src/**/*.html')
+        .pipe(dest('dist'))
+        .pipe(browserSync.stream());
+}
+
+function copy() {
+    return src(["src/img/**/*.*"])
+        .pipe(dest("dist/img"));
+}
+
+function clearDist() {
+    return src('dist', { allowEmpty: true })
+        .pipe(clean());
+}
+
+function serve() {
+    watch('src/styles/**/*.scss', buildSass);
+    watch('src/**/*.html', buildHtml);
+}
+
+function createDevServer() {
+    browserSync.init(
+        {
+            server: 'src',
+            notify: false
+        }
+    )
+}
+
+exports.build = series(clearDist, buildSass, buildHtml, copy);
+exports.default = series(buildSass, parallel(createDevServer, serve));
